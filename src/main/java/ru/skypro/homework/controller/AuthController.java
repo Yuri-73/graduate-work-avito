d6a5117d567e8fe8f89c7da3028181d7d333f8c1
Юрий <fedejkin@gmail.com>
2025-01-22 19:22:35 +0300

    Inicial commit

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -1,5 +1,7 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -8,8 +10,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.skypro.homework.dto.Login;
-import ru.skypro.homework.dto.Register;
+
+import ru.skypro.homework.dto.user.Login;
+import ru.skypro.homework.dto.user.Register;
 import ru.skypro.homework.service.AuthService;
 
 @Slf4j
@@ -20,6 +23,9 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Аутентификация пользователя")
+    @ApiResponse(responseCode = "200", description = "Успешная аутентификация")
+    @ApiResponse(responseCode = "401", description = "Неавторизованный доступ")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Login login) {
         if (authService.login(login.getUsername(), login.getPassword())) {
@@ -29,12 +35,16 @@ public class AuthController {
         }
     }
 
+    @Operation(summary = "Регистрация нового пользователя")
+    @ApiResponse(responseCode = "201", description = "Пользователь успешно зарегистрирован")
+    @ApiResponse(responseCode = "400", description = "Некорректные данные для регистрации")
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody Register register) {
-        if (authService.register(register)) {
+    public ResponseEntity<?> register(@RequestBody Register registerDTO) {
+        if (authService.register(registerDTO)) {
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 }
+
