1a8c82d54d1bb2db77746ef730ff3bf8387ccec1
Юрий <fedejkin@gmail.com>
2025-01-30 19:06:11 +0300

    Добавлена @EqualsAndHashCode для Comment

--- src/main/java/ru/skypro/homework/model/Comment.java
+++ src/main/java/ru/skypro/homework/model/Comment.java
@@ -1,6 +1,7 @@
 package ru.skypro.homework.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import java.util.Objects;
  */
 @Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "comments")
 public class Comment {
 
@@ -44,17 +46,4 @@ public class Comment {
     @ManyToOne
     @JoinColumn(name = "ad_id", referencedColumnName = "id")
     private Ad ad;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Comment comment = (Comment) o;
-        return Objects.equals(author, comment.author) && Objects.equals(authorImage, comment.authorImage) && Objects.equals(authorFirstName, comment.authorFirstName) && Objects.equals(createdAt, comment.createdAt) && Objects.equals(pk, comment.pk) && Objects.equals(text, comment.text);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(author, authorImage, authorFirstName, createdAt, pk, text);
-    }
 }
