f3d47913c0fcaa40bd863c30232763e5f8efb3ff
Юрий <fedejkin@gmail.com>
2025-02-05 20:04:34 +0300

    Добавил @Size с ограничениями для Register, Login

--- src/main/java/ru/skypro/homework/dto/user/Register.java
+++ src/main/java/ru/skypro/homework/dto/user/Register.java
@@ -3,6 +3,9 @@ package ru.skypro.homework.dto.user;
 import lombok.Data;
 import ru.skypro.homework.dto.Role;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 
 /**
  * Класс регистрации нового пользователя.
@@ -15,14 +18,19 @@ import ru.skypro.homework.dto.Role;
 @Data
 public class Register {
 
+    @Size(min = 4, max = 32)
     private String username;
 
+    @Size(min = 8, max = 16)
     private String password;
 
+    @Size(min = 3, max = 10)
     private String firstName;
 
+    @Size(min = 3, max = 10)
     private String lastName;
 
+    @Pattern(regexp = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}")
     private String phone;
 
     private Role role;
